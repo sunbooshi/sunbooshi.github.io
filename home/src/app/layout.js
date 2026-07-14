774f18b26434a9131e723891f0914aa720aec9c3
sunbooshi <sunbooshi@gmail.com>
2026-07-14 23:21:08 +0800

    remove ga.

--- home/src/app/layout.js
+++ home/src/app/layout.js
@@ -1,6 +1,5 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <GoogleAnalytics gaId="G-DMYT3S2L9X" />
     </html>
   )
 }
